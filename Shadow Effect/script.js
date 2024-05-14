540518931d29d7405f65e8bb49cc67c1e5e5aa60
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-15 00:05:34 +0530

    shadow project

---
+++ Shadow Effect/script.js
@@ -0,0 +1,20 @@
+let text = document.getElementById('text');
+let light = document.getElementById('light');
+document.addEventListener('mousemove', function(event) {
+    let mouseX = event.clientX;
+    let mouseY = event.clientY; 
+    light.style.left = mouseX + 'px';
+    light.style.top = mouseY + 'px';
+
+    let distanceX = mouseX - text.offsetLeft - text.offsetWidth / 2;
+    let distanceY = mouseY - text.offsetTop - text.offsetHeight / 2;
+
+    let newShadow = '';
+    for (let i = 0; i < 200; i++) {
+        let shadowX = -distanceX * (i / 200);
+        let shadowY = -distanceY * (i / 200);
+        let opacity = 1 - (i / 100);
+        newShadow += (newShadow ? ',' : '') + shadowX + 'px ' + shadowY + 'px 0 rgba(33,33,33,' + opacity + ')';
+    }
+    text.style.textShadow = newShadow;
+});
